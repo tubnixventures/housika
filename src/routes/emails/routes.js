e77e2257b0b60d99d8331d7670f030aff46b9e98
tubnixventures <tubnixventures@gmail.com>
2025-11-11 23:47:59 -0800

    corrected customer care email import error in src/routes/emails/routes.js

--- src/routes/emails/routes.js
+++ src/routes/emails/routes.js
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { postCustomerCareEmail } from './customercare.js';
+import { postCustomerCareEmail } from './customerCare.js';
 import { postCeoEmail } from './ceo.js';
 import { postAdminEmail } from './admin.js'; // ✅ Import Admin email handler
 
